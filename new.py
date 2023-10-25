2907bf5982cd3d8c52e13c37e8633d90129348cb
devaditya <kharedevaditya@gmail.com>
2023-10-25 12:51:34 +0530

    caps d

--- new.py
+++ new.py
@@ -100,7 +100,7 @@ def inputMatrix(matrix, rows, cols):
         matrix.append(row)
     return True
 
-# Function to take input for matrix dimensions
+# Function to take input for matrix Dimensions
 def inputDimensions():
     sz = int(input("\n Enter the size of Matrix: "))
     rows = sz
